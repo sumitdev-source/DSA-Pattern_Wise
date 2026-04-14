b01c8da8b2e8ec4d1db32df3e5e0615a8023f601
sumitdev-source <sumitg963147@gmail.com>
2026-04-14 18:07:45 +0530

    Added comment tested

--- miscellaneous_ques/LC_1.java
+++ miscellaneous_ques/LC_1.java
@@ -3,24 +3,25 @@ package miscellaneous_ques;
 import java.util.Arrays;
 
 public class LC_1 {
-   public static int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[]{i, j};
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
                 }
             }
         }
-        return new int[]{};
+        return new int[] {};
     }
 
-    public static void main(String[] args){
-      int[] arr= {1,2,5,6};
-      int target = 8;
-      int[] res = twoSum(arr, target);
-      System.out.println(Arrays.toString(res));
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 5, 6 };
+        int target = 8;
+        int[] res = twoSum(arr, target);
+        System.out.println(Arrays.toString(res));
     }
 }
 
-//it's time complexity is O(n2).
-//but i will write it in O(n).
+// it's time complexity is O(n2).
+// but i will write it in O(n).
+// tested
