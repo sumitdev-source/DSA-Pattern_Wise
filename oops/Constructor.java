5ebbb0b53ca004df014f1201a71c0cdf5d2c4096
sumitdev-source <sumitg963147@gmail.com>
2026-06-22 18:45:10 +0530

    Constructor

---
+++ oops/Constructor.java
@@ -0,0 +1,59 @@
+package oops;
+
+class B {
+  int age;
+  String name;
+  String grade;
+
+  int marks;
+  String remarks;
+
+  //default constructor, it is called automatically ,even if you dont make constructor.
+  B(){
+    System.out.println("Constructor is executed");
+  }
+
+  //if the instance variable name is different than Constructor Parameters variables,then we dont need "this" keyword.
+  B(int mark, String remark){
+      marks = mark;
+      remarks = remark;
+  }
+
+  //if the instance variable name is same as Constructor Parameters variables,then we need "this" keyword.
+  B(int age, String name, String grade){
+    this.age = age;
+    this.name = name;
+    this.grade = grade;
+  }
+
+  //copy constructor
+  B(B s){
+    age = s.age;
+    name = s.name;
+    grade = s.grade;
+  }
+
+  //+ ", marks: " + marks + ", remarks: " + remarks + 
+  public String toString(){
+    return "B{" + "age: " + age + ", name: " + name + ", Grade: " + grade + "}";
+    } 
+  }
+
+
+public class Constructor {
+  public static void main(String[] args) {
+    //1. Default constructor is called
+    B s1 = new B();
+    System.out.println(s1);
+
+    //2. Parametrized Constructor
+    B s2 = new B(4, "A", "F");
+    System.out.println(s2);
+
+    //3. Copy Constructor
+    B s3 = new B(s2);
+    System.out.println(s2);
+
+  }
+
+}
