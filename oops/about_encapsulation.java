9df8371957e9098e84e0c5199e3d7fdd4d519b59
sumitdev-source <sumitg963147@gmail.com>
2026-06-23 17:03:47 +0530

    About encapsulation

---
+++ oops/about_encapsulation.java
@@ -0,0 +1,37 @@
+package oops;
+class Student {
+  //Using getter and setter method
+  /*
+  private int age;
+  
+  // getter method
+  public int getAge(){
+    return age;
+  }
+
+  //setter method -it just set it doesnt return anything
+  public void setAge(int age){
+    this.age = age;
+  }
+  */
+  
+}
+
+public class about_encapsulation {
+  public static void main(String[] args) {
+    /*
+    Student s = new Student();
+    s.setAge(20);
+    System.out.println(s.getAge());
+    s.setAge(12365);
+    System.out.println(s.getAge());
+    */
+  }
+}
+/*
+Encapsulation: It binds togather data and behavior in the single unit i.e. class. And restrict direct access to some of the data and provide access by a public method.
+Encapsulation focuses on Data hiding and Controlled access.
+We can achieve it by getter and setter. Getter and Setter is not mandatory but it used mostly.
+
+Why Encapsulation : Data hiding, Controll Access, Improved Security.
+*/
